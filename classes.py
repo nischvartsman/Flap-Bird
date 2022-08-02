eb0a10b6d3d1b8eb536bca7b3352e64e6ec18f78
Mariana Albuquerque <marianaaa1@al.insper.edu.br>
2022-08-02 15:02:29 -0300

    classes

--- classes.py
+++ classes.py
@@ -1,24 +1,23 @@
+import pygame
 import flappy
 from configuracoes import DIR_IMG, DT, LARGURA, ALTURA 
 from os import path
 from elementos import ALTURA_G, LARGURA_G, GALINHA, CHAO
 
-class Player(flappy.sprite.Sprite):
+class Player(pygame.sprite.Sprite):
     def __init__(self, grupo):
-        flappy.sprite.Sprite.__init__(self)
-        self.imagens = []
-        for i in range(0,2):
-            self.image = flappy.image.load(path.join(DIR_IMG,GALINHA,'galinha.png'.format(i) )).convert_alpha()
-            self.image = flappy.transform.scale(self.image, (LARGURA_G, ALTURA_G))
-            self.imagens.append(self.image)
-
-        self.mask = flappy.mask.from_surface(self.image)
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.image = pygame.image.load(path.join(DIR_IMG,GALINHA,'galinha.png')).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (LARGURA_G, ALTURA_G))
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.centerx = LARGURA - LARGURA_G/2
         self.rect.bottom = CHAO
         self.speedx = 0
         self.speedy = 0
         self.groups = grupo
+        self.click = False
 
         self.y_gravidade = 2
         self.chao = CHAO
@@ -33,11 +32,8 @@ class Player(flappy.sprite.Sprite):
             self.rect.bottom = self.chao
         
     def update(self):
+        
         self.rect.x += self.speedx
         self.rect.bottom += self.speedy
 
-        if self.speedx < 0:
-            self.image = self.imagens[1]
-        if self.speedx > 0:
-            self.image = self.imagens[0]
             
