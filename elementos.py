2f8b7e0756d0520404143e2e1c71b11ec8510ef4
Mariana Albuquerque <marianaaa1@al.insper.edu.br>
2022-08-04 10:59:45 -0300

    sons

--- elementos.py
+++ elementos.py
@@ -1,6 +1,8 @@
-
+from os import path
+import pygame
 from configuracoes import ALTURA
 
+SND_DIR = path.join(path.dirname(__file__),  'sons')
 
 
 GALINHA = 'galinha.png'
@@ -18,3 +20,15 @@ ALTURA_T = 165
 OVO = 'ovo.png'
 LARGURA_OVO = 30
 ALTURA_OVO = 50
+
+def som_assets():
+    assets = {}
+
+    pygame.mixer.music.load(path.join(SND_DIR, 'fazendinha.wav'))
+    pygame.mixer.music.set_volume(0.4)
+    assets['fazendinha'] = pygame.mixer.Sound(path.join(SND_DIR, 'fazendinha.wav'))
+    assets['galinha'] = pygame.mixer.Sound(path.join(SND_DIR,'galinha.wav'))
+    assets[ 'colisao'] = pygame.mixer.Sound(path.join(SND_DIR,'colisao_galinha.wav'))
+    assets['game over'] = pygame.mixer.Sound(path.join(SND_DIR, 'game_over.wav'))
+
+    return assets
